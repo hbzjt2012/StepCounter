2ff7b83d6545bde28b8b3da7db1efd919567144b
zhaojuntao <hbzjt2012@foxmail.com>
2018-11-03 19:30:10 +0800

    add StepCounter code

--- User/main.c
+++ User/main.c
@@ -44,7 +44,228 @@ uint8_t menu_index = 1;
 
 void CountStep(void)
 {
-    /*这部分代码省略*/
+    /*计步算法*/
+	unsigned char i = 0;
+    signed short X = 0, Y = 0, Z = 0;
+    
+    READ_L3G4200D();	     //读取L3G4200D数据
+    DATA_printf(TX_DATA,T_X);
+    if(TX_DATA[0]==0x2D)
+        x=-((TX_DATA[1]&0x0f)*100+(TX_DATA[2]&0x0f)*10+(TX_DATA[3]&0x0f));
+    else
+        x=((TX_DATA[1]&0x0f)*100+(TX_DATA[2]&0x0f)*10+(TX_DATA[3]&0x0f));
+    Send_data('X');
+    DATA_printf(TX_DATA,T_Y);
+    if(TX_DATA[0]==0x2D)
+        y=-((TX_DATA[1]&0x0f)*100+(TX_DATA[2]&0x0f)*10+(TX_DATA[3]&0x0f));
+    else
+        y=((TX_DATA[1]&0x0f)*100+(TX_DATA[2]&0x0f)*10+(TX_DATA[3]&0x0f));
+    Send_data('Y');
+    DATA_printf(TX_DATA,T_Z);
+    if(TX_DATA[0]==0x2D)
+        z=-((TX_DATA[1]&0x0f)*100+(TX_DATA[2]&0x0f)*10+(TX_DATA[3]&0x0f));
+    else
+        z=((TX_DATA[1]&0x0f)*100+(TX_DATA[2]&0x0f)*10+(TX_DATA[3]&0x0f));
+    Send_data('Z');
+    printf("\r\n");
+    
+    ucData[0]=x;
+    ucData[1]=y;
+    ucData[2]=z;
+    ucData[6] = StepCountFinal;
+    X = ucData[0];
+    Y = ucData[1];
+    Z = ucData[2];
+    
+    if(m == DATASIZE)
+    {
+        m = 0;
+        if(m_count == 0)
+        {
+            m_count = 1;
+        }
+    }
+    if(n == 5) {
+        n = 0;
+    }
+    DateBufferX[n] = X;
+    DateBufferY[n] = Y;
+    DateBufferZ[n] = Z;
+    if(InitFlag < 5) {
+        GMeter.X.Data[m] = X;
+        GMeter.Y.Data[m] = Y;
+        GMeter.Z.Data[m] = Z;
+        InitFlag++;
+    } else {
+        GMeter.X.Data[m] = (DateBufferX[0] + DateBufferX[1] + DateBufferX[2] + DateBufferX[3] + DateBufferX[4])/ 5;
+        GMeter.Y.Data[m] = (DateBufferY[0] + DateBufferY[1] + DateBufferY[2] + DateBufferX[3] + DateBufferX[4])/ 5;
+        GMeter.Z.Data[m] = (DateBufferZ[0] + DateBufferZ[1] + DateBufferZ[2] + DateBufferX[3] + DateBufferX[4])/ 5;
+    }
+    if(m_count == 1) {
+        if(GMeter.X.MaxMark == m || GMeter.X.MinMark == m || GMeter.Y.MaxMark == m || GMeter.Y.MinMark == m || GMeter.Z.MaxMark == m || GMeter.Z.MinMark == m) {
+            unsigned char tempXMaxMark = GMeter.X.MaxMark;
+            unsigned char tempXMinMark = GMeter.X.MinMark;
+            unsigned char tempYMaxMark = GMeter.Y.MaxMark;
+            unsigned char tempYMinMark = GMeter.Y.MinMark;
+            unsigned char tempZMaxMark = GMeter.Z.MaxMark;
+            unsigned char tempZMinMark = GMeter.Z.MinMark;
+            if(GMeter.X.MaxMark == m) {
+                GMeter.X.Max = -200;
+            }
+            if(GMeter.X.MinMark == m) {
+                GMeter.X.Min = +200;
+            }
+            if(GMeter.Y.MaxMark == m) {
+                GMeter.Y.Max = -200;
+            }
+            if(GMeter.Y.MinMark == m) {
+                GMeter.Y.Min = +200;
+            }
+            if(GMeter.Z.MaxMark == m) {
+                GMeter.Z.Max = -200;
+            }
+            if(GMeter.Z.MinMark == m) {
+                GMeter.Z.Min = +200;
+            }
+            for(i = 0; i < DATASIZE; i++) {
+                if(GMeter.X.MaxMark == m) {
+                    if(GMeter.X.Data[i] >= GMeter.X.Max) {
+                        GMeter.X.Max = GMeter.X.Data[i];
+                        tempXMaxMark = i;
+                    }
+                }
+                if(GMeter.X.MinMark == m) {
+                    if(GMeter.X.Data[i] <= GMeter.X.Min) {
+                        GMeter.X.Min = GMeter.X.Data[i];
+                        tempXMinMark = i;
+                    }
+                }
+                if(GMeter.Y.MaxMark == m) {
+                    if(GMeter.Y.Data[i] >= GMeter.Y.Max) {
+                        GMeter.Y.Max = GMeter.Y.Data[i];
+                        tempYMaxMark = i;
+                    }
+                }
+                if(GMeter.Y.MinMark == m) {
+                    if(GMeter.Y.Data[i] <= GMeter.Y.Min) {
+                        GMeter.Y.Min = GMeter.Y.Data[i];
+                        tempYMinMark = i;
+                    }
+                }
+                if(GMeter.Z.MaxMark == m) {
+                    if(GMeter.Z.Data[i] >= GMeter.Z.Max) {
+                        GMeter.Z.Max = GMeter.Z.Data[i];
+                        tempZMaxMark = i;
+                    }
+                }
+                if(GMeter.Z.MinMark == m) {
+                    if(GMeter.Z.Data[i] <= GMeter.Z.Min) {
+                        GMeter.Z.Min = GMeter.Z.Data[i];
+                        tempZMinMark = i;
+                    }
+                }
+            }
+            GMeter.X.MaxMark = tempXMaxMark;
+            GMeter.X.MinMark = tempXMinMark;
+            GMeter.Y.MaxMark = tempYMaxMark;
+            GMeter.Y.MinMark = tempYMinMark;
+            GMeter.Z.MaxMark = tempZMaxMark;
+            GMeter.Z.MinMark = tempZMinMark;
+        }
+    }
+    if(GMeter.X.Data[m] >= GMeter.X.Max) {
+        GMeter.X.Max = GMeter.X.Data[m];
+        GMeter.X.MaxMark = m;
+    }
+    if(GMeter.X.Data[m] <= GMeter.X.Min) {
+        GMeter.X.Min = GMeter.X.Data[m];
+        GMeter.X.MaxMark = m;
+    }
+    if(GMeter.Y.Data[m] >= GMeter.Y.Max) {
+        GMeter.Y.Max = GMeter.Y.Data[m];
+        GMeter.Y.MaxMark = m;
+    }
+    if(GMeter.Y.Data[m] <= GMeter.Y.Min) {
+        GMeter.Y.Min = GMeter.Y.Data[m];
+        GMeter.Y.MinMark = m;
+    }
+    if(GMeter.Z.Data[m] >= GMeter.Z.Max) {
+        GMeter.Z.Max = GMeter.Z.Data[m];
+        GMeter.Z.MaxMark = m;
+    }
+    if(GMeter.Z.Data[m] <= GMeter.Z.Min) {
+        GMeter.Z.Min = GMeter.Z.Data[m];
+        GMeter.Z.MinMark = m;
+    }
+    GMeter.X.Base      = (GMeter.X.Max + GMeter.X.Min) / 2;
+    GMeter.Y.Base      = (GMeter.Y.Max + GMeter.Y.Min) / 2;
+    GMeter.Z.Base      = (GMeter.Z.Max + GMeter.Z.Min) / 2;
+    GMeter.X.UpLimit   = (GMeter.X.Base + GMeter.X.Max * 2) / 3;
+    GMeter.Y.UpLimit   = (GMeter.Y.Base + GMeter.Y.Max * 2) / 3;
+    GMeter.Z.UpLimit   = (GMeter.Z.Base + GMeter.Z.Max * 2) / 3;
+    GMeter.X.DownLimit = (GMeter.X.Base + GMeter.X.Min * 2) / 3;
+    GMeter.Y.DownLimit = (GMeter.Y.Base + GMeter.Y.Min * 2) / 3;
+    GMeter.Z.DownLimit = (GMeter.Z.Base + GMeter.Z.Min * 2) / 3;
+    GMeterAmax = DataSelect(&GMeter);
+    switch(GMeterAmax) {
+    case 1:
+        if((GMeter.X.Data[m] > GMeter.X.UpLimit) && StepFlag == 0) {
+            StepFlag = 1;
+        }
+        if((GMeter.X.Data[m] < GMeter.X.DownLimit) && StepFlag ==1) {
+            StepFlag = 0;
+            StepCountFinal++;
+           // StepCountFinal++;
+        }
+        break;
+    case 2:
+        if((GMeter.Y.Data[m] > GMeter.Y.UpLimit) && StepFlag == 0) {
+            StepFlag = 1;
+        }
+        if((GMeter.Y.Data[m] < GMeter.Y.DownLimit) && StepFlag ==1) {
+            StepFlag = 0;
+            StepCountFinal++;
+           // StepCountFinal++;
+        }
+        break;
+    case 3:
+        if((GMeter.Z.Data[m] > GMeter.Z.UpLimit) && StepFlag == 0) {
+            StepFlag = 1;
+        }
+        if((GMeter.Z.Data[m] < GMeter.Z.DownLimit) && StepFlag ==1) {
+            StepFlag = 0;
+            StepCountFinal++;
+          //  StepCountFinal++;
+        }
+        break;
+    default:
+        break;
+    }
+    ucData[6]   = GMeter.X.Data[m] >> 8;
+    ucData[7]   = GMeter.X.Data[m] & 0x00FF;
+    ucData[8]   = GMeter.Y.Data[m] >> 8;
+    ucData[9]   = GMeter.Y.Data[m] & 0x00FF;
+    ucData[10]  = GMeter.Z.Data[m] >> 8;
+    ucData[11]  = GMeter.Z.Data[m] & 0x00FF;
+    ucData[12]  = GMeter.X.Base >> 8;
+    ucData[13]  = GMeter.X.Base & 0x00FF;
+    ucData[14]  = GMeter.X.UpLimit >> 8;
+    ucData[15]  = GMeter.X.UpLimit & 0x00FF;
+    ucData[16]  = GMeter.X.DownLimit >> 8;
+    ucData[17]  = GMeter.X.DownLimit & 0x00FF;
+    ucData[18]  = GMeter.X.Max >> 8;
+    ucData[19]  = GMeter.X.Max & 0x00FF;
+    ucData[20]  = GMeter.X.Min >> 8;
+    ucData[21]  = GMeter.X.Min & 0x00FF;
+    ucData[22]  = StepCountFinal;
+    ucData[23]  = StepFlag;
+    ucData[24]  = 0;
+    ucData[25]  = 0;
+    m++;
+    n++;
+
+//    OLED_DisNum(1u,2u,ucData[22],3,16,LineYes);
+		printf("CountSteps: %i \r\n",(uint16_t)ucData[22]);
 }
 
 void PowerOn(void)
